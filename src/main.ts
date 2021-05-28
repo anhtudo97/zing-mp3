507de0c35bbafd3e05a0a65ed333b5ade59511e6
anhtudo97 <anhtudo97@gmail.com>
2021-05-28 14:26:07 +0700

    feat: add multiple lang

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import PerfectScrollbar from 'vue3-perfect-scrollbar'
 import VueTippy from 'vue-tippy'
+import VueI18n from '@/locales'
 import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
 
 import App from './App.vue'
@@ -8,4 +9,5 @@ import App from './App.vue'
 const app = createApp(App)
 app.use(PerfectScrollbar)
 app.use(VueTippy)
+app.use(VueI18n)
 app.mount('#app')
