9a42e9cfbbaaa14492f586aded3fed0961a3b1b7
anhtudo97 <anhtudo97@gmail.com>
2021-06-06 22:10:13 +0700

    chore: update router

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { createApp } from 'vue'
 import PerfectScrollbar from 'vue3-perfect-scrollbar'
 import VueTippy from 'vue-tippy'
-import VueI18n from '@/locales'
 import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
 
 import App from './App.vue'
+import routes from './routes'
+import store from './store'
+
+import './assets/styles/app.scss'
+import 'tippy.js/dist/tippy.css'
 
 const app = createApp(App)
+app.use(store)
+app.use(routes)
 app.use(PerfectScrollbar)
 app.use(VueTippy)
-app.use(VueI18n)
 app.mount('#app')
